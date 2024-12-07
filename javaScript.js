3ef1bc27e3553ef2594d622300bf37dc3be05ac9
JosueCodeBit <josue.art.inf@gmail.com>
2024-12-07 03:33:12 -0300

    Add files via upload

---
+++ javaScript.js
@@ -0,0 +1,20 @@
+// scripts.js
+
+/*let points = 0;
+
+function earnPoints() {
+    points += 10;
+    document.getElementById('point-count').innerText = points;
+    updateBadge();
+}
+
+function updateBadge() {
+    let badgeDisplay = document.getElementById('badge-display');
+    if (points >= 50) {
+        badgeDisplay.innerText = "Fan de Lang Lang";
+        badgeDisplay.style.color = "gold";
+    } else {
+        badgeDisplay.innerText = "Ninguna";
+        badgeDisplay.style.color = "black";
+    }
+}*/
